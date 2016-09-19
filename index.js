1a843712a69fbb7f27d569f75668f3aa7e30c4ca
emersion <contact@emersion.fr>
2016-09-19 19:10:31 +0200

    Listen once to messages, use public keys instead of swarm ids, use brain

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 'use strict'
 
 const swarm = require('discovery-swarm')
+const brain = require('brain')
 const Node = require('./lib/node')
 const Unit = require('./lib/unit')
 const BrainUnit = require('./lib/brain-unit')
 const Executor = require('./lib/executor')
+const trained = require('./trained')
 
 const topic = 'magi'
-
-const names = ['Melchior', 'Balthasar', 'Casper']
-const units = []
+const names = ['melchior', 'balthasar', 'casper']
 
 function createSwarm() {
 	const sw = swarm()
@@ -18,30 +18,42 @@ function createSwarm() {
 	return sw
 }
 
+const units = []
+
 names.forEach((name, i) => {
 	const sw = createSwarm()
 
-	const unit = new Unit(sw)
+	const net = new brain.NeuralNetwork()
+	net.fromJSON(trained[i])
+
+	const unit = new BrainUnit(sw, net, 'black')
 	units.push(unit)
 
-	unit.decide = question => false
-
-	console.log('Created unit', i+1, name, sw.id)
+	console.log('Created unit', i+1, name, unit.id)
 })
 
 const exec = new Executor(createSwarm(), 'selfdestruct')
-console.log('Created executor', exec.swarm.id)
+console.log('Created executor', exec.id)
 
 const hud = new Node(createSwarm())
-console.log('Created HUD', hud.swarm.id)
+console.log('Created HUD', hud.id)
 
-setTimeout(() => {
+Promise.all(units.concat(exec, hud).map(node => {
+	return new Promise((resolve, reject) => {
+		node.on('connected', () => {
+			resolve()
+		})
+	})
+})).then(() => {
+	console.log('Network is fully connected')
+
 	const question = {
 		id: '0',
-		module: 'selfdestruct'
+		module: 'selfdestruct',
+		data: { r: 0.95, g: 0.02, b: 0.43 }
 	}
 
 	hud.publish('question', question)
 
 	console.log('Started poll:', question)
-}, 1000)
+})
